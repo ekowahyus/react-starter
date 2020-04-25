f0b7d3e9c15d3f6504f490e3baecad080d98abc5
eko wahyu s <eko.wahyu@garasi.id>
2020-04-25 20:04:15 +0700

    virtual dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { el, diff, patch } from 'simple-virtual-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+let count = 0;
+
+const renderTree = () => {
+    count++
+
+    let items = []
+    let color = (count % 2 === 0) ? 'blue': 'red'
+
+    for (let i = 0; i < count; i++) {
+        items.push(
+            el('li', ['Item ke ' + i])
+        )
+    }
+
+    return el('div', {'id': 'container'}, [
+        el('h1', {style: 'color: ' + color}, ['Virtual DOM']),
+        el('p', ['Hitungan ke: ' + count]),
+        el('ul', items)
+    ])
+}
+
+// generate real DOM
+let tree = renderTree()
+let root = tree.render()
+
+document.body.appendChild(root)
+
+setInterval(() => {
+    let newTree = renderTree()
+    let patches = diff(tree, newTree)
+    
+    patch(root, patches)
+
+    tree = newTree
+}, 1000)
